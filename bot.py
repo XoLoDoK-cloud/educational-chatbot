603828444a31b4fadc40a2fe22a7daf160d3c5b5
XoLoDoK-cloud <lucky.kuvol@gmail.com>
2025-11-22 19:31:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -196,3 +196,18 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
