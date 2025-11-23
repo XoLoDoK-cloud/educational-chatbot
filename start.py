3f4fdd83ca339c70902d9a8aab5a49f6c74ef065
XoLoDoK-cloud <lucky.kuvol@gmail.com>
2025-11-23 05:09:24 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
-import subprocess
-import sys
-
-# Устанавливаем зависимости
-print("📦 Installing dependencies...")
-subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
-
-# Запускаем бота
-print("🚀 Starting bot...")
+import asyncio
 from bot import main
+
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
