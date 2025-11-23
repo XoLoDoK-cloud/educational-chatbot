7e86a1df8605d0e50264d5894c4523a1bb7a82c6
XoLoDoK-cloud <lucky.kuvol@gmail.com>
2025-11-23 05:04:53 +0300

    Delete test_ai.py

--- test_ai.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from ai_openrouter import openrouter_ai
-
-async def test():
-    response = await openrouter_ai.generate_response("пушкин", "Привет! Как твои дела?")
-    print("Ответ ИИ:", response)
-
-asyncio.run(test())
